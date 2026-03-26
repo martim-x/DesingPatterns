4271ed7d82d8572c4f86210abaf2bbab5b773faf
martim-x <timofeymarushko@gmail.com>
2026-03-26 11:05:23 +0300

    completed lab3

---
+++ term2/lab3/ICelebrity.lib/ICelebrity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ICelebrity.lib;
+
+public interface ICelebrity<T> : IDisposable
+{
+    List<T> GetAll();
+    T? GetById(int id);
+
+    bool Add(T celebrity);
+    bool DelById(int id);
+    bool UpdateById(int id, T celebrity);
+
+    int AddAndGetId(T celebrity);
+
+    int GetIdByName(string celebrityName);
+
+    int SaveChanges();
+}
---
+++ term2/lab3/Test/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using ICelebrity.lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Repository.JSONFileName = "Celebrities.json";
+
+        using (ICelebrity<Celebrity> repo = Repository.Create("Celebrities"))
+        {
+            Console.WriteLine("-------------- GetAll ----------------------");
+            List<Celebrity> celebrities = repo.GetAll();
+            celebrities.ForEach(c =>
+                Console.WriteLine(
+                    $"Id = {c.Id}, Firstname = {c.FirstName}, Surname = {c.Surname}, PhotoPath = {c.PhotoPath} "
+                )
+            );
+
+            Console.WriteLine("-------------- Add ---------------------------");
+
+            repo.Add(new Celebrity(0, "Marvin", "Minsky", "/Photo/Minsky.jpg"));
+            celebrities = repo.GetAll();
+            celebrities.ForEach(c =>
+                Console.WriteLine(
+                    $"Id = {c.Id}, Firstname = {c.FirstName}, Surname = {c.Surname}, PhotoPath = {c.PhotoPath} "
+                )
+            );
+
+            Console.WriteLine("-------------- GetIdByName, UpdateById ---------------------------");
+
+            int id = repo.GetIdByName("Marvin");
+            if (id >= 0)
+            {
+                repo.UpdateById(id, new Celebrity(id, "Marvin", "Minsky", "/Photo/Minsky.jpg"));
+            }
+            celebrities = repo.GetAll();
+            celebrities.ForEach(c =>
+                Console.WriteLine(
+                    $"Id = {c.Id}, Firstname = {c.FirstName}, Surname = {c.Surname}, PhotoPath = {c.PhotoPath} "
+                )
+            );
+
+            Console.WriteLine("-------------- GetIdByName, DelById ---------------------------");
+
+            id = repo.GetIdByName("Minsky");
+            if (id >= 0)
+            {
+                repo.DelById(id);
+            }
+            celebrities = repo.GetAll();
+            celebrities.ForEach(c =>
+                Console.WriteLine(
+                    $"Id = {c.Id}, Firstname = {c.FirstName}, Surname = {c.Surname}, PhotoPath = {c.PhotoPath} "
+                )
+            );
+
+            Console.WriteLine("-------------- AddAndGetId ---------------------------");
+
+            id = repo.AddAndGetId(new Celebrity(0, "Marvin", "Minsky", "/Photo/Minsky.jpg"));
+            Console.WriteLine($"New Id = {id}");
+            celebrities = repo.GetAll();
+            celebrities.ForEach(c =>
+                Console.WriteLine(
+                    $"Id = {c.Id}, Firstname = {c.FirstName}, Surname = {c.Surname}, PhotoPath = {c.PhotoPath} "
+                )
+            );
+
+            Console.WriteLine("SaveChanges = {0}", repo.SaveChanges());
+        }
+
+        using (ICelebrity<Celebrity> repo = Repository.Create("Celebrities"))
+        {
+            List<Celebrity> celebrities = repo.GetAll();
+            celebrities.ForEach(c =>
+                Console.WriteLine(
+                    $"Id = {c.Id}, Firstname = {c.FirstName}, Surname = {c.Surname}, PhotoPath = {c.PhotoPath} "
+                )
+            );
+        }
+    }
+}
